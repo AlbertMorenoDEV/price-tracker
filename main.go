d0bba7bd06005d54631004e7fa3b30b47ad23524
Albert Moreno <albert.moreno@letgo.com>
2019-12-03 14:13:42 +0100

    Adding concurrency V2

--- main.go
+++ main.go
@@ -3,51 +3,83 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"os"
+	"log"
 	"strings"
+	"time"
 
 	"github.com/anaskhan96/soup"
 )
 
-type data struct {
-	Title string
-	Price float64
-}
+type (
+	pageUrl string
+	data    struct {
+		Title string
+		Price float64
+	}
+	response struct {
+		dat data
+		err error
+	}
+)
 
-var p = []string{
-"https://www.vivobarefoot.com/es/mens/everyday/gobi-ii-mens?colour=Black",
-"https://www.vivobarefoot.com/eu/mens/outdoor/primus-trail-fg-mens-ow5?colour=Charcoal",
-"https://www.vivobarefoot.com/eu/mens/outdoor/tracker-fg-mens?colour=Dark+Brown",
-"https://www.vivobarefoot.com/eu/mens/active/primus-lite-mens-ow5?colour=Obsidian",
-"https://www.vivobarefoot.com/eu/mens/outdoor/primus-trail-sg-mens?colour=Black",
-"https://www.vivobarefoot.com/eu/mens/everyday/ra-ii-mens?colour=Midnight+Navy",
-"https://www.vivobarefoot.com/eu/mens/active/primus-knit-lux-mens?colour=Black",
-"https://www.vivobarefoot.com/eu/mens/active/primus-knit-mens?colour=Mood+Indigo",
+var pagesUrl = []pageUrl{
+	"https://www.vivobarefoot.com/es/mens/everyday/gobi-ii-mens?colour=Black",
+	"https://www.vivobarefoot.com/eu/mens/outdoor/primus-trail-fg-mens-ow5?colour=Charcoal",
+	"https://www.vivobarefoot.com/eu/mens/outdoor/tracker-fg-mens?colour=Dark+Brown",
+	"https://www.vivobarefoot.com/eu/mens/active/primus-lite-mens-ow5?colour=Obsidian",
+	"https://www.vivobarefoot.com/eu/mens/outdoor/primus-trail-sg-mens?colour=Black",
+	"https://www.vivobarefoot.com/eu/mens/everyday/ra-ii-mens?colour=Midnight+Navy",
+	"https://www.vivobarefoot.com/eu/mens/active/primus-knit-lux-mens?colour=Black",
+	"https://www.vivobarefoot.com/eu/mens/active/primus-knit-mens?colour=Mood+Indigo",
 }
 
 func main() {
-	n := len(p)
-	d := make(chan data, n)
+	workc := make(chan pageUrl, 8)
+	resultc := make(chan response, 8)
 
-	go getAllUrls(d)
+	go func() {
+		for {
+			select {
+			case res := <-resultc:
+				if res.err != nil {
+					log.Println(res.err)
+					continue
+				}
 
-	for r := range d {
-		fmt.Println(r)
+				fmt.Println(res.dat)
+			default:
+			}
+		}
+	}()
+
+	for i := 0; i < 4; i++ {
+		go func() {
+			for {
+				select {
+				case pag := <-workc:
+					resultc <- getResponse(pag)
+				default:
+				}
+			}
+		}()
 	}
-}
 
-func getAllUrls(d chan data) {
-	for _, u := range p {
+	for _, u := range pagesUrl {
 		fmt.Println("URL: " + u)
-		d <- getData(u)
+		workc <- u
+
+		time.Sleep(5 * time.Second)
 	}
-	close(d)
+
+	fmt.Println("Done!")
 }
 
-func getData(u string) data {
-	resp, err := soup.Get(u)
+func getResponse(u pageUrl) response {
+	resp, err := soup.Get(string(u))
 	if err != nil {
-		os.Exit(1)
+		return response{
+			err: err,
+		}
 	}
 	doc := soup.HTMLParse(resp)
 
@@ -61,11 +93,16 @@ func getData(u string) data {
 		} `json:"defaults"`
 	}{}
 	if err := json.Unmarshal([]byte(dataFpAttr), &res); err != nil {
-		panic(err)
+		return response{
+			err: err,
+		}
 	}
 
-	return data{
-		Title: title,
-		Price: res.Defaults.Eur,
+	return response{
+		dat: data{
+			Title: title,
+			Price: res.Defaults.Eur,
+		},
+		err: nil,
 	}
 }
